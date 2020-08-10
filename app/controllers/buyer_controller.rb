fbbc618b414a9f1159f7746a63160b7cdf29fd8c
THOMAS <thomasjfisher1@gmail.com>
2020-08-10 14:29:37 +1000

    Updated backend functions, added some styling, preparing for heroku upload

--- app/controllers/buyer_controller.rb
+++ app/controllers/buyer_controller.rb
@@ -2,6 +2,10 @@ class BuyerController < ApplicationController
   def page
     #find the listing they are buying
     #update buyer id of listing
-    # @listing.buyer_id = current_user.profile.id
+
+    @listing = Listing.find(params[:listing])
+
+    @listing.buyer_id = current_user.profile.id
+    @listing.save
   end
 end
